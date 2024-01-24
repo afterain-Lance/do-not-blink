8a564179d1f7740be2b9963504eec1bec076c3cd
Hyungyu Jo <112043061+afterain-Lance@users.noreply.github.com>
2024-01-24 17:07:41 +0900

    :construction: Quater

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const play = async () => {
+    const backgroundVideo = document.getElementById('background-video');
     document.getElementById('bgm').removeEventListener('play', play);
     await wait(200);
     const 이것은 = document.getElementById('1');
@@ -127,7 +128,7 @@ const play = async () => {
     await wait(450);
     document.body.style.backgroundColor = 'black'
     countdown.classList.remove('active')
-    await wait(300);
+    await wait(200);
     const alabama = document.getElementById('alabama');
     alabama.classList.add('active')
     await wait(10);
@@ -148,13 +149,13 @@ const play = async () => {
     alabama.innerText = '향해';
     await wait(225);
     alabama.innerText = '갑니다';
-    await wait(225);
-    const alabamaVideo = document.getElementById('alabama-video');
-    alabama.classList.remove('active')
-    alabamaVideo.classList.add('active');
-    alabamaVideo.play();
+    await wait(325);
+    alabama.classList.remove('active');
+    backgroundVideo.src = '/alabama.mp4';
+    backgroundVideo.classList.add('active');
+    backgroundVideo.play();
     await wait(2400);
-    alabamaVideo.classList.remove('active');
+    backgroundVideo.classList.remove('active');
     alabama.classList.add('active');
     alabama.innerText = '팀이';
     await wait(200);
@@ -166,12 +167,12 @@ const play = async () => {
     await wait(200);
     alabama.innerText = '올라갑니다';
     await wait(200);
-    const stageVideo = document.getElementById('stage-video');
     alabama.classList.remove('active')
-    stageVideo.classList.add('active');
-    stageVideo.play();
+    backgroundVideo.classList.add('active');
+    backgroundVideo.src = '/stage.mp4';
+    backgroundVideo.play();
     await wait(500);
-    stageVideo.classList.remove('active');
+    backgroundVideo.classList.remove('active');
     alabama.classList.add('active');
     alabama.innerText = '그가';
     await wait(200);
@@ -198,7 +199,7 @@ const play = async () => {
     await wait(100);
     hello.style.transition = 'transform 50ms';
     hello.style.transform = 'translateX(-3rem)';
-    await wait(50);
+    await wait(100);
     hello.style.transform = '';
     hello.style.fontSize = '10rem';
     hello.style.transition = '';
@@ -215,12 +216,12 @@ const play = async () => {
     hello.style.fontSize = '200rem';
     await wait(250);
     document.body.style.backgroundColor = 'black';
-    await wait(200);
+    await wait(100);
     hello.classList.remove('active');
     document.body.style.transition = '';
     const introduce = document.getElementById('introduce');
     introduce.classList.add('active');
-    await wait(200);
+    await wait(100);
     introduce.style.fontSize = '4rem';
     await wait(200);
     introduce.innerText = '여러분들께';
@@ -228,16 +229,174 @@ const play = async () => {
     introduce.innerText = '보여';
     await wait(400);
     introduce.innerText = '보여드릴';
-    await wait(300);
+    await wait(325);
     introduce.innerText = '보여드릴 것이';
-    await wait(300);
+    await wait(325);
     introduce.innerText = '있습니다';
-    await wait(300);
+    await wait(200);
     introduce.style.opacity = 0;
+    wait(1500).then(() => {
+        introduce.classList.remove('active');
+    });
+    backgroundVideo.classList.add('active');
+    backgroundVideo.src = '/iphone 7 first show.mp4';
+    backgroundVideo.play();
+    wait(5600).then(() => {
+        backgroundVideo.classList.remove('active');
+    });
+    await wait(3000);
+    const desc1 = document.getElementById('desc-1');
+    const desc2 = document.getElementById('desc-2');
+    desc1.classList.add('active');
+    await wait(10);
+    desc1.style.opacity = 1;
+    await wait(1500);
+    desc1.style.opacity = 0;
+    desc1.style.transform = 'translateX(-4rem)';
+    await wait(10);
+    desc2.classList.add('active');
+    await wait(10);
+    desc2.style.opacity = 1;
+    desc2.style.transform = '';
+    wait(250).then(() => {
+        desc1.classList.remove('active');
+    });
+    await wait(1000);
+    desc2.style.opacity = 0;
+    desc2.classList.remove('active');
+    await wait(10);
+    const bigSeven = document.getElementById('big-seven');
+    bigSeven.classList.add('active');
+    document.body.style.backgroundColor = 'white';
+    await wait(250);
+    bigSeven.classList.remove('active');
+    document.body.style.backgroundColor = 'black';
+    const sevenSpelling = document.getElementById('seven-spelling')
+    sevenSpelling.classList.add('active-flex');
+    await wait(250);
+    const [sp1, sp2, sp3, sp4, sp5, sp6, sp7] = sevenSpelling.getElementsByTagName('span');
+    sp2.innerText = 'i';
+    sp5.innerText = 't';
+    // sp7.style.scale = 0;
+    // sp7.style.opacity = 0;
+    // wait(50).then(() => {
+    sp7.style.display = 'none';
+    // })
+    await wait(250);
+    // sp2.style.scale = 0;
+    // sp2.style.opacity = 0;
+    sp4.innerText = 'p';
+    // sp6.style.scale = 0;
+    // sp6.style.opacity = 0;
+    // wait(50).then(() => {
+    sp2.style.display = 'none';
+    sp6.style.display = 'none';
+    // })
+    await wait(250);
+    sp1.innerText = '일'
+    sp3.innerText = '곱';
+    sp4.style.display = 'none';
+    sp5.style.display = 'none';
+    await wait(250);
+    sevenSpelling.classList.remove('active-flex');
+    document.body.style.backgroundColor = 'white';
+    const sevenDesc1 = document.getElementById('seven-desc-1');
+    const [sd1_1, sd1_2] = sevenDesc1.getElementsByTagName('span');
+    const sevenDesc2 = document.getElementById('seven-desc-2');
+    const [sd2_1, sd2_2, sd2_3] = sevenDesc2.getElementsByTagName('span');
+    sevenDesc1.classList.add('active-flex');
+    sd1_1.style.opacity = 1;
+    await wait(250);
+    sd1_2.style.opacity = 1;
+    await wait(250);
+    sevenDesc1.classList.remove('active-flex');
+    sevenDesc2.classList.add('active-flex');
+    sd2_1.style.opacity = 1;
+    await wait(250);
+    sd2_2.style.opacity = 1;
+    await wait(250);
+    sd2_3.style.opacity = 1;
+    await wait(250);
+    sevenDesc2.style.scale = 'calc(2 / 3)';
+    await wait(250);
+    sevenDesc2.classList.remove('active-flex');
+    const sevenDesc3 = document.getElementById('seven-desc-3');
+    document.body.style.backgroundColor = 'black';
+    sevenDesc3.classList.add('active');
+    await wait(250);
+    sevenDesc3.innerText = '블랙 블랙.'
+    await wait(250);
+    sevenDesc3.classList.remove('active');
+    backgroundVideo.classList.add('active');
+    backgroundVideo.src = '/iphone 7 2.mp4';
+    backgroundVideo.play()
+    await wait(300);
+    backgroundVideo.classList.remove('active');
+    const sevenDesc4 = document.getElementById('seven-desc-4');
+    const [sd4_1, sd4_2, sd4_3] = sevenDesc4.getElementsByTagName('span');
+    sevenDesc4.classList.add('active-flex');
+    sd4_1.style.opacity = 1;
+    await wait(125);
+    sd4_2.style.opacity = 1;
+    await wait(125);
+    sd4_1.style.opacity = 0;
+    await wait(125);
+    sd4_3.style.opacity = 1;
+    await wait(125);
+    sd4_2.style.opacity = 0;
+    await wait(125);
+    sd4_3.style.opacity = 0;
+    await wait(125);
+    sevenDesc4.classList.remove('active-flex');
+    const sevenDesc5 = document.getElementById('seven-desc-5');
+    const [sd5_1, sd5_2] = sevenDesc5.getElementsByTagName('span');
+    sevenDesc5.classList.add('active-flex');
+    sd5_1.style.opacity = 1;
+    await wait(125);
+    sd5_2.style.opacity = 1;
+    await wait(125);
+    sevenDesc5.classList.remove('active-flex');
+    backgroundVideo.classList.add('active');
+    backgroundVideo.src = '/iphone 7 3.mp4';
+    backgroundVideo.play()
+    await wait(300);
+    backgroundVideo.classList.remove('active');
+    sevenDesc5.classList.remove('active-flex');
+    const sevenDesc6 = document.getElementById('seven-desc-6');
+    const [sd6_1, sd6_2, sd6_3] = sevenDesc6.getElementsByTagName('span');
+    sevenDesc6.classList.add('active-flex');
+    sd6_1.style.opacity = 1;
+    await wait(125);
+    sd6_2.style.opacity = 1;
+    await wait(125);
+    sd6_3.style.opacity = 1;
+    await wait(125);
+    sevenDesc6.classList.remove('active-flex');
+    const sevenDesc7 = document.getElementById('seven-desc-7');
+    const [sd7_1, sd7_2] = sevenDesc7.getElementsByTagName('span');
+    const [sd7_2_1, sd7_2_2] = sd7_2.getElementsByTagName('span');
+    sevenDesc7.classList.add('active-flex');
+    sd7_1.style.opacity = 1;
+    sd7_2.style.maxWidth = '0%';
+    await wait(125);
+    sevenDesc7.style.transform = '';
+    sd7_2.style.display = 'flex';
+    await wait(50);
+    sd7_2.style.maxWidth = '100%';
     await wait(600);
+    sd7_1.style.opacity = 0;
+    sd7_2_2.style.opacity = 0;
+    wait(200).then(() => {
+        sevenDesc7.classList.remove('active-flex');
+    });
+    backgroundVideo.classList.add('active');
+    backgroundVideo.src = '/iphone 7 4.mp4';
+    backgroundVideo.play()
+    await wait(1400);
+    backgroundVideo.classList.remove('active');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('bgm').addEventListener('play', play);
-    setTimeout(() => {document.getElementById('bgm').play();}, 500)
+    setTimeout(() => { document.getElementById('bgm').play(); }, 500)
 });
